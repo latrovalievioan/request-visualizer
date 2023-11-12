3de0d0568c684da8c30d45420e24363bc418d131
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-12 16:25:02 +0300

    setup context with useReducer

--- src/components/RequestBar/SendButton.tsx
+++ src/components/RequestBar/SendButton.tsx
@@ -1,3 +1,31 @@
+import * as React from "react";
+import { RequestMethods } from "../../constants";
+import { Context } from ".";
+
+const sendXHR = (method: RequestMethods, url: string) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log(this.responseText);
+    }
+  };
+
+  switch (method) {
+    case RequestMethods.get:
+      xhr.open(method, url, true);
+      xhr.send("");
+      break;
+    default:
+      alert(`${method} method not implemented yet`);
+  }
+};
+
 export const SendButton = () => {
-  return <button>Send</button>;
+  const context = React.useContext(Context);
+
+  if (!context) return null;
+
+  const { state } = context;
+
+  return <button onClick={() => sendXHR(state.method, state.url)}>Send</button>;
 };
